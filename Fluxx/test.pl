a678bb00df0a7ce15fbe83138bb598470fd7508b
istamenov <screwgarena@abv.bg>
2015-09-05 14:48:37 +0300

    all

---
+++ Fluxx/test.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use v5.18;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use Fluxx::Engine::Card;
+use Fluxx::Engine::Player;
+my $Card = Fluxx::Engine::Card->new('Name' => 'Test', 
+                                     'Type' => 'Test',
+                                     'Description' => 'Test',
+                                     'Reference' => 'Test',
+                                     'Effect' => sub{return 43});
+my $Player = Fluxx::Engine::Player->new('Name' => 'TestPN',
+                                        'Hand' => [$Card ],
+                                        'Keepers' => [],
+                                        'Creepers' => []);
+say $Player->Name;
+say @{$Player->Hand}[0]->Name;
+
+
+use Fluxx::Engine::Rules;
+
+my $Rules = Fluxx::Engine::Rules->new;
+
+say $Rules->HandLimit;
+
+my $Board = Fluxx::Engine::Rules->new('DrawPile' => [],
+                                      'DiscardPile' => [],
+                                      'GoalAndRUlesCards' => [],
+                                      'Players' => [$Player]);
